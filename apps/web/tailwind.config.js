0186d7a30baf0be3ef04d8b4da53d730e5a414c5
Deekshith Rathod <68671324+Deekshithrathod@users.noreply.github.com>
2023-12-05 14:53:26 +0530

    add tailwind config

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,1 +1,19 @@
-module.exports = require("@quoto/configs/tailwind/tailwind.config");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	...require("@quoto/configs/tailwind/tailwind.config"),
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+		// for UI from componentss
+		"../../packages/ui/src/**/*{.js,.ts,.jsx,.tsx}",
+
+		// Or if using `src` directory:
+		"./src/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
