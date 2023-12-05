650916584397057d7b49bd8b66be2b3972e2e4f4
Deekshith Rathod <68671324+Deekshithrathod@users.noreply.github.com>
2023-12-05 16:43:36 +0530

    update skeleton quote

--- packages/ui/src/skeletons/QuoteSkeleton.tsx
+++ packages/ui/src/skeletons/QuoteSkeleton.tsx
@@ -1,13 +1,11 @@
 const QuoteSkeleton = () => {
 	return (
-		<div className="text-xl lg:text-4xl w-full lg:max-w-3xl border-l-[#F7DF94] pl-10 border-l-8 mx-auto  flex flex-col gap-2">
-			"
-			<p className="animate-pulse w-full h-4 lg:h-10 bg-slate-300 rounded-md"></p>
-			<p className="animate-pulse w-full h-4 lg:h-10 bg-slate-300 rounded-md"></p>
+		<div className="text-xl lg:text-4xl w-full lg:max-w-3xl border-l-[#F7DF94] pl-10 border-l-8 mx-auto  flex flex-col gap-1">
+			<span>"</span>
 			<p className="animate-pulse w-full h-4 lg:h-10 bg-slate-300 rounded-md"></p>
 			<p className="animate-pulse w-full h-4 lg:h-10 bg-slate-300 rounded-md"></p>
 			<p className="animate-pulse w-1/2  h-4 lg:h-10 bg-slate-300 rounded-md"></p>
-			"
+			<span>"</span>
 		</div>
 	);
 };
